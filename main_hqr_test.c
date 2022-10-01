bada25839a24acfffa7a193786422c0f76918ad4
Johnathan Rhyne <rhynejohnathan@gmail.com>
2022-10-01 13:23:13 -0600

    changed codebase to use a macro to reference matrix elements

--- main_hqr_test.c
+++ main_hqr_test.c
@@ -4,6 +4,21 @@
 #include <math.h>
 #define _PB_N n
 #define SCALAR_VAL(x) x
+#define b0(i,j) B[i + j * n]
+#define b1(i,j) B[(i - 1) + (j - 1) * n]
+#define a0(i,j) A[i + j * n]
+#define a1(i,j) A[(i - 1) + (j - 1) * n]
+
+
+// This may be bad practice, but we put all malloc'd entities
+// as global variables in order to make freeing the
+// memory easier
+double* A;
+double* B;
+double* wr;
+double* wi;
+double* eigenValsReal;
+double* eigenValsImag;
 
 /*
  * This is header is for the subroutine inside hqr.f
@@ -17,7 +32,7 @@ extern void hqr_( int* nm, int* n, int* low, int* igh,
 
 void usage()
 {
-    printf("main_hqr_test.exe [-n sizeOfMatrix, -v]\n");
+    printf("main_hqr_test.exe [-n sizeOfMatrix | -v | -h]\n");
     printf("\t-n: The following argument must be a positive integer\n");
     printf("\t\tThe default value is 20\n");
     printf("\t-v: verbose flag that prints out the results of eispack hqr\n");
@@ -25,19 +40,20 @@ void usage()
     printf("\t-h: Print this help dialogue\n");
 }
 
-void freeMemory(void *A, void *B, void*wr, void*wi){
+void freeMemory(){
     free(A);
     free(B);
     free(wr);
     free(wi);
+    free(eigenValsReal);
+    free(eigenValsImag);
 }
 
 int main(int argc, char ** argv) {
 	/*
-	 * Declaring variables to pass into the hqr subroutine
+	 * Declaring variables to pass into the hqr_ subroutine
 	 */
 	int i, ierr, j, n;
-	double *A, *B, *wr, *wi;
 	// Store the constant 1 as a variable to be sent into hqr_
 	int ione=1;
         
@@ -84,20 +100,20 @@ int main(int argc, char ** argv) {
 	// Generate A as a random matrix.
  	for(i = 0; i < n; i++)
  		for(j = 0; j < n; j++)
-			A[i+j*n] = (double)rand() / (double)(RAND_MAX) - 0.5e+00;
+			a0(i,j) = (double)rand() / (double)(RAND_MAX) - 0.5e+00;
 	
 	// since hqr needs A to be upper hessenberg, we will set all other values 
 	// to 0	in order find the eigenvalues using other sources
 	for ( int i = 2; i < n; i++ ) {
 		for (int j = 0; j < i - 1; j++) {
-			A[i + j*n] = 0;
+                    a0(i,j) = 0;
 		}
 	}
         // Store a copy of A into B so that we can run our own
         // version of hqr written in C 
         for ( int i = 0; i < n; i++ )
             for ( int j = 0; j < n; j++ )
-                B[i +j*n] = A[i + j*n];
+                b0(i,j) = a0(i,j);
 	/*
 	 * Here, we print out A for finding the eigenvalues via MATLAB
 	 * This must be done before calling hqr_ because it destroys A
@@ -106,9 +122,9 @@ int main(int argc, char ** argv) {
             printf("A = [\n");
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n - 1; j++) {
-                    printf("%f,", A[i+j*n]);
+                    printf("%f,", a0(i,j));
                 }
-                printf("%f\n", A[i + (n-1)*n]);
+                printf("%f\n", a0(i,n-1));
             }
             printf("]\n");
         }
@@ -132,27 +148,27 @@ int main(int argc, char ** argv) {
         // a C implementation of EISPACK's HQR.f where we move from an upper
         // Hessenberg matrix to the Schur form.
         
-        double *eigenValsReal, *eigenValsImag;
         eigenValsReal = (double *) malloc(n * sizeof(double));
         eigenValsImag = (double *) malloc(n * sizeof(double));
 
-        int indexOfError = 0;
+        int indexOfError = 1;
         double norm = 0;
-        int k = 0;
+        int k = 1;
 
-        int low = 0;
-        int igh = n-1;
+        // These deal with our boundary conditions
+        int low = 1;
+        int igh = n;
 
-        for (int i = 0; i < n; i++ ){
-            for (int j = k; j < n; j++) 
-                norm += abs(B[i + j * n]); // 1 norm of the matrix A
+        for (int i = 1; i <= n; i++ ){
+            for (int j = k; j <= n; j++) 
+                norm += abs(b1(i,j)); // 1 norm of the matrix A
             k = i;
             // This never executes, however it is functionality in the 
-            // original hwr subroutine, so I am repeating it here
+            // original hqr subroutine, so I am repeating it here
             // in case there is a chance of balanc being reimplemented as well.
             if ( i <= low || i >= igh) {
-                wr[i] = B[i + i*n];
-                wi[i] = 0;
+                eigenValsReal[i] = b1(i,i);
+                eigenValsImag[i] = 0;
             }
         }
         int en = igh;
@@ -169,32 +185,33 @@ int main(int argc, char ** argv) {
             int na = en -1;
             int enm2 = na -1;
 
-            int l = 0;
+            // Another index counter
+            int l = 1;
             // Look for single small sub-diagonal element 
-            for (int i = low; i <= en; i++) {
-                l = en + low - i;
+            for (int ll = low; ll <= en; ll++) {
+                l = en + low - ll;
                 if (l != low) {
-                    double s = abs(B[(l-1) + (l-1)*n]) + abs(B[l + l*n]);
+                    double s = abs(b1(l-1,l-1)) + abs(b1(l,l));
                     if (s == 0) //consider using another method of checking if s is 0 
                         s = norm;
                     double tst1 = s;
-                    double tst2 = tst1 + abs(abs(B[l + (l-1)*n]));
+                    double tst2 = tst1 + abs(abs(b1(l,l-1)));
                     // Found it?
                     if (tst1 == tst2)
                         break;
                 }
             }
             // Form shift
-            x = B[en + en*n];
+            x = b1(en,en);
             // Single root found. Why?
             if (l == en) {
-                wr[en] = x + t;
-                wi[en] = 0;
+                eigenValsReal[en] = x + t;
+                eigenValsImag[en] = 0;
                 en = na;
                 continue;
             }
-            y = B[na + na*n];
-            w = B[en + na*n] * B[na + en*n];
+            y = b1(na,na);
+            w = b1(en,na) * b1(na,en);
 
             //Double root found. Why?
             if (l == na) {
@@ -205,29 +222,29 @@ int main(int argc, char ** argv) {
                 if (q >= 0) {
                     // Real pair
                     zz = (p >= 0)?(p + zz):(p - zz);
-                    wr[na] = x + zz;
-                    wr[en] = (zz == 0)?(wr[na]):(x - w / zz);
-                    wi[na] = 0;
-                    wi[en] = 0;
+                    eigenValsReal[na] = x + zz;
+                    eigenValsReal[en] = (zz == 0)?(eigenValsReal[na]):(x - w / zz);
+                    eigenValsImag[na] = 0;
+                    eigenValsImag[en] = 0;
                 } else {
                     // Complex pair
-                    wr[na] = x + p;
-                    wr[en] = x + p;
-                    wi[na] = zz;
-                    wi[en] = -zz;
+                    eigenValsReal[na] = x + p;
+                    eigenValsReal[en] = x + p;
+                    eigenValsImag[na] = zz;
+                    eigenValsImag[en] = -zz;
                 }
             } else if (itn == 0){
                 // We have not converged to all eigenvalues after 30*n iterations.
                 ierr = en;
                 // To prevent memory leaks
-                freeMemory(A,B,wr,wi);
+                freeMemory();
                 return 1;
             } else if (its == 10 || its == 20) {
                 // form exceptional shift
                 t += x;
                 for (int j = low; j <= en; j++) 
-                    B[i+i*n] -= x;
-                s = abs(B[en + na*n]) + abs(B[na + enm2*n]);
+                    b1(i,i) -= x;
+                s = abs(b1(en,na)) + abs(b1(na,enm2));
                 x = s * 0.75;
                 y = x;
                 w = -.4375 * s * s;
@@ -238,50 +255,54 @@ int main(int argc, char ** argv) {
             // for m=en-2 to l in step of -1
             int m;
             for (m = enm2; i >= l; i--) {
-                zz = B[m+m*n];
+                zz = b1(m,m);
                 r = x - zz;
                 s = y - zz;
-                p = (r*s-w) / B[(m+1) + m*n] + B[m + (m+1)*n];
-                q = B[m+1 + (m+1)*n] - zz - r - s;
-                r = B[m+2 + (m+1)*n];
+                p = (r * s - w) / b1(m + 1, m) + b1(m, m + 1);
+                q = b1(m + 1, m + 1) - zz - r - s;
+                r = b1(m + 2, m + 1);
                 s = abs(p) + abs(q) + abs(r);
                 p /= s;
                 q /= s;
                 r /= s;
-                if (m != l) {
-                    tst1 = abs(p)*(abs(B[(m-1) + (m-1)*n]) + abs(zz) + abs(B[m+1 + (m+1)*n]));
-                    tst2 = tst1 + abs(B[m + (m-1)*n])*(abs(q) + abs(r));
+                // We only do the following checks if we are not on the 
+                // last iteration of this loop
+                if (m != l) { 
+                    tst1 = abs(p)*(abs(b1(m - 1, m - 1)) + abs(zz) + abs(b1(m + 1, m + 1)));
+                    tst2 = tst1 + abs(b1(m, m - 1))*(abs(q) + abs(r));
                     // Why not just test if right part of tst2 is 0?
                     if (tst1 == tst2)
                         break;
-                }
+                } 
             }
             for (int i = m-2;i <= en;i++) {
-                B[i + (i-2)*n] = 0;
+                b1(i, i - 2) = 0;
                 if (i != m - 2)
-                    B[i + (i-3)*n] = 0;
+                    b1(i, i - 3) = 0;
             }
             // double qr step involving rows l to en and columns m to en
             for (int k = m; k <= na; k++) {
+                // If na == k, then we are on our
+                // last iteration, and use this to determine
+                // if we do some actions or not
                 int lastIter = (na - k == 0)?(0):(1);
+                // As long as we are not on our first iteration
                 if ( k != m ) {
-                    p = B[k + (k-1)*n];
-                    q = B[k+1 + (k-1)*n];
-                    r = (!lastIter) ? (B[(k+2) + (k-1)*n]):(0);
+                    p = b1(k, k - 1);
+                    q = b1(k + 1, k - 1);
+                    r = (!lastIter) ? (b1( k + 2, k - 1)):(0);
                     x = abs(p) + abs(q) + abs(r);
                     if ( x == 0 )
                         continue;
                     p /= x;
                     q /= x;
                     r /= x;
-                    s = sqrt(p*p + q*q + r*r);
                 }
-                if (p < 0)
-                    s *= -1;
+                s = (p < 0) ? (-sqrt(p*p + q*q + r*r)) : (sqrt(p*p + q*q + r*r));
                 if ( k != m ) {
-                    B[k + (k-1)*n] = x * -s;
+                    b1(k, k - 1) = x * -s;
                 } else if (l != m){
-                    B[k + (k-1)*n] = -B[k + (k-1)*n];
+                    b1(k, k - 1) *= -1;
                 }
                 p += s;
                 x = p/s;
@@ -291,33 +312,39 @@ int main(int argc, char ** argv) {
                 r /= p;
                 // Row modification
                 for (int j=k; j<=en; j++) {
-                        p = B[k+j*n] + q * B[k+1+j*n];
-                        B[k+j*n] -= p*x;
-                        B[k+1+j*n] -= p*y;
-                        if (!lastIter)
-                            B[k+2+j*n] -= p*y*zz;
+                    if (lastIter)
+                        p = b1(k, j) + q * b1(k + 1, j);
+                    else
+                        p = b1(k, j) + q * b1(k + 1, j) + r * b1(k + 2, j);
+                    b1(k,j) -= p*x;
+                    b1(k + 1, j) -= p*y;
+                    if (!lastIter)
+                        b1(k + 1, j) -= p * zz;
                 }
-                    // Column modification
+                // Column modification
                 int colModMax = (en < k+3) ? (en):(k+3);
                 for (int i = l; i<= colModMax;i++) {
-                    p = x*B[i+k*n] + y * B[i + (k+1)*n];
-                    B[i+k*n] -= p;
-                    B[i+(k+1)*n] -= p*q;
+                    if (lastIter)
+                        p = x * b1(i, k) + y * b1(i, k + 1);
+                    else
+                        p = x * b1(i, k) + y * b1(i, k + 1) + zz * b1(i , k + 2);
+                    b1(i, k) -= p;
+                    b1(i, k + 1) -= p*q;
                     if (!lastIter)
-                        B[i+(k+2)*n] -= p*r;
+                        b1(i, k + 2) -= p*r;
                 }
 
             }
         }
 
         if (printFlag) {
-            printf("wr = [\n");
+            printf("eigValsReal = [\n");
             for ( int i = 0; i < n; i++)
-                printf("    %5.8f,\n", wr[i]);
+                printf("    %5.8f,\n", eigenValsReal[i]);
             printf("]\n");
-            printf("wi = [\n");
+            printf("eigValsImag = [\n");
             for ( int i = 0; i < n; i++)
-                printf("    %5.8f,\n", wi[i]);
+                printf("    %5.8f,\n", eigenValsImag[i]);
             printf("]\n");
         }
 
@@ -325,6 +352,6 @@ int main(int argc, char ** argv) {
         //Now, we free the memory and then exit
 
         // Freeing memory
-        freeMemory(A,B,wr,wi);
+        freeMemory();
 	return 0;
 }
