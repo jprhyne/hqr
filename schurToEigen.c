88310e169339c8dbd51dfb090ab0881cfe04dd35
Johnathan Rhyne <rhynejohnathan@gmail.com>
2022-12-30 12:07:20 -0700

    updated schurToEigen.h

--- schurToEigen.c
+++ schurToEigen.c
@@ -1,9 +1,4 @@
-#include<math.h>
-#define t1(i,j) T[(i - 1) + (j - 1) * n]
-#define eigenMatrix1(i,j) eigenMatrix[(i - 1) + (j - 1) * n]
-
-extern void cdiv(double ar, double ai, double br, double bi, double *cr, double *ci); 
-
+#include"schurToEigen.h"
 /**
  * Input:
  *
