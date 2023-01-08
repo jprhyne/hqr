ac4f77ddd8543c6858afea9b87e5007e8892928d
Julien Langou <julien.langou@ucdenver.edu>
2023-01-07 17:41:26 -0700

    ping

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/env bash
-numIter=50
-numStep=1
+numIterN=100
+numStepN=1
 
-for k in $(seq 1 "$numStep" "$numIter"); do
-	for seed in $(seq 1 "$numStep" "$numIter"); do
+numIterSeed=50
+numStepSeed=1
+
+for k in $(seq 1 "$numStepN" "$numIterN"); do
+	for seed in $(seq 1 "$numStepSeed" "$numIterSeed"); do
 		echo -n "seed=$seed,   " 
-		./test_hqr2eigen_fortran.exe -n $k -s $seed
+		#./test_hqr2eigen_fortran.exe -n $k -s $seed
+		./test_schurVectors.exe -n $k -s $seed
 	done
 done
